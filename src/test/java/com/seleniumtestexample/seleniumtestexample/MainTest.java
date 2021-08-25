b7175e60dc3eae3eb79b0e4e661b45032353081d
Gaetani <deniscgaetani@gmail.com>
2021-08-25 14:35:56 -0300

    initial commit

--- src/test/java/com/seleniumtestexample/seleniumtestexample/MainTest.java
+++ src/test/java/com/seleniumtestexample/seleniumtestexample/MainTest.java
@@ -1,2 +1,32 @@
-package com.seleniumtestexample.seleniumtestexample;public class MainTest {
+package com.seleniumtestexample.seleniumtestexample;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MainTest {
+    private WebDriver driver;
+    String link = "https://ultimateqa.com/automation";
+
+    @BeforeAll
+    public static void setupDriver() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    void openQAWebsite() {
+        Assertions.assertEquals("https://ultimateqa.com/automation", link);
+        driver.get("https://ultimateqa.com/automation");
+    }
 }
